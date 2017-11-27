4f11a0fb09b20861c6fcd54f5aa560115fbc2094
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-27 11:54:06 +0000

    Refactor test names for readability

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -3,11 +3,11 @@ var readBook = App.readBook;
 var removePunct = App.removePunct;
 
 describe("Book", () =>{
-    it("can output the contents of a book", () => {
+    it("can output it's text contents", () => {
         expect(readBook('/Users/frederickofori/Documents/TechTests/Word-Count/test.txt')).toEqual('This test works.');
     });
 
-    it("can remove all punctuation from the book", function(){
+    it("can remove all punctuation", function(){
         var sampleBook = "This! t^est&* isn't isn't wor-king?"
         expect(removePunct(sampleBook)).toEqual("This test isnt isnt working");
     });
