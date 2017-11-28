2e1ab85c65fa345b58356b5be7d3870b456b487f
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-28 14:09:54 +0000

    Allows the calculation of prime numbers

--- spec/word.spec.js
+++ spec/word.spec.js
@@ -5,8 +5,19 @@ describe("Word", function(){
         var word = new Word("test", 1)
         expect(word.name).toBeDefined();
     });
+
     it("has a number property", function(){
         var word = new Word("test", 1)
         expect(word.number).toBeDefined();
     });
+
+    it("can calculate whether a number is a prime number", function(){
+        var word1 = new Word("test1", 7);
+        var word2 = new Word("test2", 9);
+        word1.isPrime();
+        word2.isPrime();
+        expect(word1.prime).toEqual(true);
+        expect(word2.prime).toEqual(false);
+    });
+
 });
--- src/word.js
+++ src/word.js
@@ -1,6 +1,18 @@
 var Word = function(name, number){
     this.name = name;
     this.number = number
+    this.prime;
+};
+
+Word.prototype.isPrime = function() {
+    for(var i = 2; i < this.number; i++) {
+        if(this.number % i === 0) {
+            this.prime = false;
+            return false;
+        }
+    }
+    this.prime = true;
+    return true;
 };
 
 module.exports = {
