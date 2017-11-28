82624a0e2d1eab9b778b8066a8548248bf118292
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-28 11:37:04 +0000

    Create Book and move functions into prototype design pattern

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
 const fs = require('fs');
 
-var readBook = function(book){
-    return fs.readFileSync(book, 'utf8');
-};
-
-var removePunct = function(book){
-    return book.replace(/[.,\/#!?@$'%\^&\*;:{}=\-_`~()]/g,"");
-};
-
-var removeCap = function(book){
-    return book.toLowerCase();
-};
-
-var splitWords = function(book){
-    return book.split(" ");
-};
-
-var removeSpaces = function(book){
-    return book.replace(/\r?\n|\r/g," ");
+var Book = function(){
+    this.content;
+    this.wordsArray;
 }
 
-var removeDup = function(array){
-    return array.filter(function(curr, index, arr) {
+Book.prototype.readBook = function(book){
+    var bookString = fs.readFileSync(book, 'utf8');
+    this.content = bookString;
+    return bookString;
+};
+
+Book.prototype.removePunct = function(book){
+    var newString = book.replace(/[.,\/#!?@$'%\^&\*;:{}=\-_`~()]/g,"");
+    this.content = newString;
+    return newString;
+};
+
+Book.prototype.removeCap = function(book){
+    var newString = book.toLowerCase();
+    this.content = newString;
+    return newString;
+};
+
+Book.prototype.removeSpaces = function(book){
+  var newString = book.replace(/\s{2,}/g," ").replace(/\r?\n|\r/g," ");
+  this.content = newString;
+  return newString;
+};
+
+Book.prototype.splitWords = function(book){
+    var arr = book.split(" ");
+    this.wordsArray = arr;
+    return arr;
+};
+
+Book.prototype.removeDup = function(array){
+    var newArr = this.wordsArray.filter(function(curr, index, arr) {
         return index == arr.indexOf(curr);
-    })
+    });
+    this.wordsArray = newArr;
+    return this.wordsArray;
 };
 
 module.exports = {
-    readBook,
-    removePunct,
-    splitWords,
-    removeSpaces,
-    removeDup,
-    removeCap
+   Book
 };
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,39 +1,38 @@
-const App = require('../app.js');
-var readBook = App.readBook;
-var removePunct = App.removePunct;
-var removeSpaces = App.removeSpaces;
-var splitWords = App.splitWords;
-var removeDup = App.removeDup;
-var removeCap = App.removeCap;
+const Book = require('../app.js').Book;
 
 describe("Book", function(){
+    var book;
+    beforeAll(function() {
+        book = new Book()
+      });
+
     it("can output it's text contents", function(){
-        expect(readBook('/Users/frederickofori/Documents/TechTests/Word-Count/test.txt')).toEqual('This test works.');
+        expect(book.readBook('/Users/frederickofori/Documents/TechTests/Word-Count/test.txt')).toEqual('This test works.');
     });
 
     it("can remove all punctuation", function(){
         var sampleBook = "This! t^est&* isn't isn't wor-king?";
-        expect(removePunct(sampleBook)).toEqual("This test isnt isnt working");
+        expect(book.removePunct(sampleBook)).toEqual("This test isnt isnt working");
     });
 
     it("can remove capitalisation", function(){
         var sampleBook = "THIS test Is WorkING";
-        expect(removeCap(sampleBook)).toEqual("this test is working");
+        expect(book.removeCap(sampleBook)).toEqual("this test is working");
     });
 
     it("can remove line breaks", function(){
         var sampleBook = "This test works\n\nThis test doesnt work\n\nSTOP";
-        expect(removeSpaces(sampleBook)).toEqual("This test works This test doesnt work STOP")
+        expect(book.removeSpaces(sampleBook)).toEqual("This test works This test doesnt work STOP")
     });
 
     it("can place all the indivual words into an array", function(){
         var sampleBook = "This test isnt isnt working";
-        expect(splitWords(sampleBook)).toEqual(['This', 'test', 'isnt', 'isnt', 'working']);
+        expect(book.splitWords(sampleBook)).toEqual(['This', 'test', 'isnt', 'isnt', 'working']);
     });
 
     it("can remove duplicate values from array", function(){
         var sampleArray = ['This', 'test', 'isnt', 'This', 'isnt', 'working'];
-        expect(removeDup(sampleArray)).toEqual(['This', 'test', 'isnt', 'working']);
+        expect(book.removeDup(sampleArray)).toEqual(['This', 'test', 'isnt', 'working']);
     });
 
 });
