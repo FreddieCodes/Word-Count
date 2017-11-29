a2aee2c1ba14ad96e6fa200feda5c412d9e68f9f
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-29 01:48:39 +0000

    Refactor test description for readability

--- spec/controller.spec.js
+++ spec/controller.spec.js
@@ -1,7 +1,7 @@
 const controller = require('../src/controller.js').controller;
 
 describe("Controller", () => {
-    it("can output the word, number and if the number is a prime of each item in array", () => {
+    it("can output the word, count and if the word count is a prime of each item in array", () => {
         const test = controller.init('/Users/frederickofori/Documents/TechTests/Word-Count/test.txt')
         expect(test).toEqual('Word | Count | Prime\nthis | 1 | false\ntest | 1 | false\nworks | 1 | false\n');
     });
